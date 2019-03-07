d0f20450a146a4767e56c94cc58138222c9c4b89
Tim Goll <tim@timgoll.de>
2019-03-07 13:24:10 +0100

    removed debug stuff

--- solutions/task_0_example/run.m
+++ solutions/task_0_example/run.m
@@ -10,8 +10,6 @@ path_arr = strsplit(mfilename('fullpath'), {'/', '\'});
 task_name = string(path_arr(end-1));
 disp('running ' + task_name);
 
-a = 5;
-
 %GET SIM DATA
 simOut = sim('system_SOLENOID_r2016a', 'SimulationMode', 'normal');
 bla = simOut.get('bla');
