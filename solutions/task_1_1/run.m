cae2f088980b190ba4f8e85ea616c9d7eab239ad
Tim Goll <tim@timgoll.de>
2019-03-07 15:19:05 +0100

    added more plots to task 1/2

--- solutions/task_1_1/run.m
+++ solutions/task_1_1/run.m
@@ -16,20 +16,24 @@ disp('running ' + task_name);
 simOut_default = simulate('system_SOLENOID_default', do_rerun);
 default_displacement = simOut_default.get('displacement');
 default_time         = simOut_default.get('time');
-paw_default({default_time}, {default_displacement}, {'default'}, 'time [s]', 'displacement [m]', task_name, "solonoid default", "plots", false, true)
-
+default_force        = simOut_default.get('force');
 
 simOut_no_volatage = simulate('system_SOLENOID_no_voltage', do_rerun);
 no_volatage_displacement = simOut_no_volatage.get('displacement');
 no_volatage_time         = simOut_no_volatage.get('time');
-paw_default({no_volatage_time}, {no_volatage_displacement}, {'no volatage'}, 'time [s]', 'displacement [m]', task_name, "solonoid no voltage", "plots", false, true)
-
 
 simOut_working = simulate('system_SOLENOID_working', do_rerun);
 working_displacement = simOut_working.get('displacement');
 working_time         = simOut_working.get('time');
+working_force        = simOut_working.get('force');
+
+
+paw_default({default_time}, {default_displacement}, {'default'}, 'time [s]', 'displacement [m]', task_name, "solonoid default", "plots", false, true)
+paw_default({no_volatage_time}, {no_volatage_displacement}, {'no volatage'}, 'time [s]', 'displacement [m]', task_name, "solonoid no voltage", "plots", false, true)
 paw_default({working_time}, {working_displacement}, {'working'}, 'time [s]', 'displacement [m]', task_name, "solonoid working", "plots", false, true)
 
+paw_default({default_time}, {default_force}, {'default force plot'}, 'time [s]', 'force [N]', task_name, "solonoid default force plot", "plots", false, true)
+paw_default({working_time}, {working_force}, {'working force plot'}, 'time [s]', 'force [N]', task_name, "solonoid working force plot", "plots", false, true)
 
 
 
